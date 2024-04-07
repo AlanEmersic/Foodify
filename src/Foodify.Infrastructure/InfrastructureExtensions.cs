30c7b4d334b68a5f162ac0cdb1a22b6cd89d6047
Alan Emeršić <alan.emersic@gmail.com>
2024-04-07 20:28:10 +0200

    Extensions and DI

---
+++ src/Foodify.Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Foodify.Infrastructure;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+}
